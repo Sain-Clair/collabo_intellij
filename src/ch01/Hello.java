df29d607cb5dc43c2a96f6791570b8f81e23c0bd
green <green@naver.com>
2023-09-22 10:15:29 +0900

    2개의 파일이 수정 되었습니다.

--- src/ch01/Hello.java
+++ src/ch01/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello01");
         System.out.println("Hello02");
+        System.out.println("Hello03");
     }
 }
 
--- src/ch01/World.java
+++ src/ch01/World.java
@@ -2,8 +2,7 @@ package ch01;
 
 public class World {
     public static void main(String[] args) {
-
+        System.out.println("World01");
         System.out.println("World01");
     }
-
 }
